f5fe4a57809669d12382f554f9899c96bab57ff2
AshrafWael <ashrafwael582@gmail.com>
2024-10-10 03:12:38 +0300

    Add User Role

--- UserAccountMangment/obj/Debug/net8.0/UserAccountMangment.AssemblyInfo.cs
+++ UserAccountMangment/obj/Debug/net8.0/UserAccountMangment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserAccountMangment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420a1d05db8d9bd6d70437d590df913ee611494c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserAccountMangment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserAccountMangment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
