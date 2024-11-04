96dee687827d92e5abe9838227571c2353a16f51
AshrafWael <ashrafwael582@gmail.com>
2024-11-04 23:08:16 +0200

    add jwtOption Class

---
+++ UserAccountMangment/ConfigrationOptions/JwtOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace UserAccountMangment.ConfigrationOptions
+{
+    public class JwtOptions
+    { 
+        public string Key { get; set; }
+        public string Audince { get; set; }
+        public string issuer { get; set; }
+        public int lifeTime { get; set; }
+    }
+
+}
