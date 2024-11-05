644883be48e3205db4a2cefee698e9ae76d73bb5
AshrafWael <ashrafwael582@gmail.com>
2024-11-05 20:15:30 +0200

    add Permission DBset

--- UserAccountMangment/DbHelper/AccountContext.cs
+++ UserAccountMangment/DbHelper/AccountContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Numerics;
+using UserAccountMangment.Models;
 
 namespace UserAccountMangment.DbHelper
 {
@@ -9,6 +10,16 @@ namespace UserAccountMangment.DbHelper
     {
         public AccountContext(DbContextOptions<AccountContext> options) : base(options) { }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<UserPermission>  userPermissions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.Entity<UserPermission>().ToTable("UserPermission")
+                .HasKey(X=> new { X.UserId ,X.PermissionId });
+
+            base.OnModelCreating(builder);
+        }
 
     }
 }
