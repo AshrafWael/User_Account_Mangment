94c2a0aa1cfcbed80cd02fc5de4bb21d02440257
AshrafWael <ashrafwael582@gmail.com>
2024-11-05 20:14:34 +0200

    Check Permission Filter

---
+++ UserAccountMangment/Authentication/CheckPermissionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using UserAccountMangment.Models;
+
+namespace UserAccountMangment.Authentication
+{
+    [AttributeUsage(AttributeTargets.Method,AllowMultiple = true)]
+    public class CheckPermissionAttribute :Attribute
+    {
+        private readonly Permission _permission;
+        public CheckPermissionAttribute( Permission permission)
+        {
+           _permission = permission;
+        }
+        public Permission Permission { get; }
+    }
+}
